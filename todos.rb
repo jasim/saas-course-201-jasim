9f629c7f4af067f931ec7a54f370bcfa5e7a9e95
Jasim A Basheer <jasim.ab@gmail.com>
2020-01-11 15:12:25 +0530

    Add my first todo.rb to git

---
+++ todos.rb
@@ -0,0 +1,16 @@
+# Write a comment
+# puts and gets
+# \n\n
+# String Interpolation
+# variables
+
+puts "abcd efgh #{1 + 10}"
+
+puts "What is your next todo?"
+next_todo = gets
+
+# In the imddle of the program
+
+puts "\n\n\n" # Carriage Return
+
+puts "[ ] #{next_todo} #{next_todo}" # String Interpolation
